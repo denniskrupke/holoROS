a8b510a32e10615b967ac735b0ced1f4ddd0fe71
dennis <krupke@informatik.uni-hamburg.de>
2018-11-20 10:57:02 +0100

    Judith 2 days working on the clicker-based command input as an alternative to voice commands.

---
+++ Assets/Scripts/clickerManager.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+
+
+
+public class clickerManager : MonoBehaviour, IInputClickHandler, IInputHandler{
+
+    public GameObject MessageClicker = null;
+    public SetTransform setTransform = null;
+
+
+    [SerializeField]
+    StateController sc;
+
+    // Use this for initialization
+    void Start () {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "start";
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "clicked";
+        if (sc.CurrentState.GetType() == typeof(Idle_State))
+        {
+            sc.PreviousState = sc.CurrentState;
+            // Triggers pick-point extraction, message creation and enqueueing            
+            this.setTransform.OnPickUp();
+            if ((sc.CurrentState.GetType() == typeof(PlannedPick_State)) || (sc.CurrentState.GetType() == typeof(Idle_State))) sc.CurrentState.SetNext(true);
+            //          sc.CurrentState.SetNext(true);
+        }
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "clicked";
+    }
+    public void OnInputUp(InputEventData eventData)
+    {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "clicked";
+    }
+
+    void _execute()
+    {
+        // Triggers execution of previously planned actions            
+        if (sc.CurrentState.GetType() == typeof(PlannedPick_State))
+        {
+            this.setTransform.OnConfirmPick();
+            sc.CurrentState.SetNext(true);
+        }
+        else if (sc.CurrentState.GetType() == typeof(PlannedPlace_State))
+        {
+            this.setTransform.OnConfirmPlace();
+            sc.CurrentState.SetNext(true);
+        }
+
+    }
+
+
+}
---
+++ Assets/clickerManager.cs
@@ -0,0 +1,63 @@
+﻿
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+
+
+public class ClickerManager : MonoBehaviour, IInputClickHandler, IInputHandler{
+
+    public GameObject MessageClicker = null;
+    public SetTransform setTransform = null;
+
+
+    [SerializeField]
+    StateController sc;
+
+    // Use this for initialization
+    void Start () {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "start";
+
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        MessageClicker.GetComponent<UnityEngine.UI.Text>().text = "clicked";
+        if (sc.CurrentState.GetType() == typeof(Idle_State))
+        {
+            sc.PreviousState = sc.CurrentState;
+            // Triggers pick-point extraction, message creation and enqueueing            
+            this.setTransform.OnPickUp();
+            if ((sc.CurrentState.GetType() == typeof(PlannedPick_State)) || (sc.CurrentState.GetType() == typeof(Idle_State))) sc.CurrentState.SetNext(true);
+            //          sc.CurrentState.SetNext(true);
+        }
+
+        else if (sc.CurrentState.GetType() == typeof(PlannedPick_State))
+        {
+            this.setTransform.OnConfirmPick();
+            sc.CurrentState.SetNext(true);
+        }
+        else if (sc.CurrentState.GetType() == typeof(PlannedPlace_State))
+        {
+            this.setTransform.OnConfirmPlace();
+            sc.CurrentState.SetNext(true);
+        }
+        else
+        {
+            sc.PreviousState = sc.CurrentState;
+            // Triggers place-point extraction, message creation and enqueueing            
+            this.setTransform.OnPlace();
+            if ((sc.CurrentState.GetType() == typeof(PlannedPlace_State)) || (sc.CurrentState.GetType() == typeof(Picked_State))) sc.CurrentState.SetNext(true);
+        }
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        return;
+    }
+    public void OnInputUp(InputEventData eventData)
+    {
+        return;
+    }
+
+ 
+}
